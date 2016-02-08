7efc61fb90c5c3612c4e9c1ee1bc3a3635e8775d
Andreas Blaesius <skate4life@gmx.de>
2016-02-08 22:06:51 +0100

    don't include recovery files in bootimage
    Change-Id: I185047486461014673ea470b7908cb544b269285

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -12,12 +12,12 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := init.recovery.$(TARGET_DEVICE)
-LOCAL_SRC_FILES := init.recovery.yukon.rc
+LOCAL_SRC_FILES := ../recovery/root/init.recovery.yukon.rc
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_STEM := init.recovery.$(TARGET_DEVICE)
 LOCAL_MODULE_SUFFIX := .rc
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
