6b3f6041caaf87b243ecaa5cb9c2da233e9e672f
Erik Castricum <erikcas1972@gmail.com>
2016-04-08 00:32:29 +0200

    yukon: define and update custombootimg
    * custombootimg was not used but also not removed.
    * we need it again, update it. And add barrier
    * Tell config we must use it
    
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I336b1608a77b2923fd7a6c735bc347410f532976

--- platform_omni.mk
+++ platform_omni.mk
@@ -1,4 +1,4 @@
-# Copyright 2014 The Android Open Source Project
+# Copyright (C) 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,17 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/sony/yukon-common/PlatformConfig.mk
+# Common config
+include device/sony/yukon-common/platform.mk
 
-BOARD_CUSTOM_BOOTIMG_MK := device/sony/yukon-common/boot/custombootimg.mk
-TARGET_DTB_EXTRA_FLAGS := --force-v2
-
-# Use device's audio_effects.conf
-TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
-
-# TWRP flags
-TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
-TW_THEME := portrait_hdpi
-TW_HAS_NO_RECOVERY_PARTITION := true
-TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_NO_USB_STORAGE := true
+# dtbtool
+PRODUCT_PACKAGES += \
+    dtbTool
